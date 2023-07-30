0080f0549937340b1a192bebf29bc82e551e7874
X39 <marco@silipo.de>
2023-07-31 00:00:01 +0200

    improved wiki page design and fixed double loading issue

--- source/X39.UnitedTacticalForces.WebApp/ExtensionMethods/BreakpointExtensions.cs
+++ source/X39.UnitedTacticalForces.WebApp/ExtensionMethods/BreakpointExtensions.cs
@@ -10,10 +10,16 @@ public static class BreakpointExtensions
             ? "calc(100dvh - 64px - 52px - 16px - 64px - 64px - 64px)"
             : "calc(100dvh - 64px - 52px)";
     }
-    public static string ToFullPageHeight(this Breakpoint self)
+    public static string ToFullPageHeightWithBigQuestionmarks(this Breakpoint self)
     {
         return self is Breakpoint.Sm or Breakpoint.Xs
             ? "calc(100dvh - 16px - 64px - 64px - 64px)"
-            : "100vh";
+            : "100dvh";
+    }
+    public static string ToFullPageHeight(this Breakpoint self)
+    {
+        return self is Breakpoint.Sm or Breakpoint.Xs
+            ? "calc(100dvh - 64px)"
+            : "100dvh";
     }
 }
