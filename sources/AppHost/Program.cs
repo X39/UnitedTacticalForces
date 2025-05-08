f80ee8527a2599f23602aecbd42538e8e49eb93b
Marco Silipo <marco@silipo.de>
2025-05-08 22:17:16 +0200

    state

--- sources/AppHost/Program.cs
+++ sources/AppHost/Program.cs
@@ -6,6 +6,8 @@ var db = builder.AddPostgres("pg-database")
 var api = builder.AddProject<Projects.Api>("api")
     .WithReference(db)
     .WaitFor(db);
+var app = builder.AddProject<Projects.WebApp>("app")
+    .WaitFor(api);
 
 builder.Build()
     .Run();
